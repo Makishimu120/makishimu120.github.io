c17a3766bb77bc0148f16b13275407b4c7fb784e
Makishimu120 <74831656+Makishimu120@users.noreply.github.com>
2025-12-13 08:21:23 +0300

    Update Ph.js

--- lampa/Ph.js
+++ lampa/Ph.js
@@ -1,2 +1,2 @@
-pornhub.com##div[id*=age-verification-wrapper]
-pornhub.com##div[id*=age-verification-container]
+pornhub.org##div[id*=age-verification-wrapper]
+pornhub.org##div[id*=age-verification-container]
